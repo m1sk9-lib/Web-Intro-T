f471f474e75f2c8afeff8041dc0248add3d43050
Sho Sakuma <me@m1sk9.dev>
2025-07-15 16:28:26 +0900

    feat: Add confim page

--- src/register.php
+++ src/register.php
@@ -1,12 +1,12 @@
 <head>
-    <title>登録完了 | ブックマーク共有システム</title>
+    <title>登録 | ブックマーク共有システム</title>
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+JP&display=swap" rel="stylesheet">
 </head>
 <body>
-    以下のサイトを登録しました． <br>
+    以下の内容で登録します．よろしいですか？ <br>
     <?php
         $sitename = $_GET["inputSitename"];
         $url = $_GET["inputURL"];
@@ -17,5 +17,7 @@
         print("ジャンル: " . $genre . "<br>");
     ?>
     <hr />
-    <input type="button" value="戻る" onclick="history.back()">
+    <!-- TODO: 登録処理後に一覧ページに遷移する -->
+    <input type="button" value="登録する" onclick="alert('登録しました'); location.href='index.html'">
+    <input type="button" value="修正" onclick="history.back()">
 </body>
